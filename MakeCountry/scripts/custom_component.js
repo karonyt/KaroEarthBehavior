0ae4f1ee8c0b62947ba5ec2bf40dbf703cd24d0e
karonyt <tomatotest34@gmail.com>
2024-08-19 17:41:08 +0900

    update custom_component.js

--- MakeCountry/scripts/custom_component.js
+++ MakeCountry/scripts/custom_component.js
@@ -7,9 +7,16 @@ world.beforeEvents.worldInitialize.subscribe((ev) => {
         onRandomTick(ev) {
             const growth_stage = ev.block.permutation.getState('mc:growth_stage');
             if (growth_stage < 3) {
+                const randomNum = RandomInt(1, 20);
+                if (randomNum != 2) return; 
                 const new_permutation = ev.block.permutation.withState('mc:growth_stage', growth_stage + 1);
                 ev.block.setPermutation(new_permutation);
             }
         },
     });
 });
+
+
+function RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
