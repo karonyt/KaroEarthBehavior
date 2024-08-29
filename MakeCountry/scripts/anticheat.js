ddaf01b4149d2c909e7de7693003f1a2749e2d22
karonyt <tomatotest34@gmail.com>
2024-08-29 18:46:57 +0900

    update anticheat.js

--- MakeCountry/scripts/anticheat.js
+++ MakeCountry/scripts/anticheat.js
@@ -2,31 +2,91 @@ import { world, Player, system, GameMode, EquipmentSlot } from "@minecraft/serve
 import { ModalFormData, FormCancelationReason } from "@minecraft/server-ui"
 import { ChestFormData } from "./lib/chest-ui";
 import { itemIdToPath } from "./texture_config";
+import * as DyProp from "./lib/DyProp";
 
 const unbanList = [];
 const banList = [`giyokundaK`, `bedrock6gatu`, `D Berlin98`, `reizylake`, `Harukixx1008`];
 const unmuteList = [];
 const muteList = [];
 
-world.afterEvents.playerSpawn.subscribe((ev) => {
+const rawDeviceIds = DyProp.getDynamicProperty("deviceIds") || "[]";
+/**
+ * @type {[string]}
+ */
+const deviceIds = JSON.parse(rawDeviceIds);
+
+world.afterEvents.playerSpawn.subscribe(async (ev) => {
     const { player } = ev;
+    /**
+     * @type {{"command": "listd", "result": [{ "deviceSessionId": string , "id": number , "xuid": string }]}}
+     */
+    const data = await JSON.parse(executeCommand(`listd`, true).split(`\n`)[2].substring(5));
+    const playerData = data.result.find((d) => `${d.id}` === player.id);
+    if (!playerData) {
+        player.runCommand(`kick "${player.name}" §c§l不正なアカウント`);
+        world.sendMessage(`§a§l[KaronNetWork BAN System]\n§r§7不正なアカウント: ${player.name} の接続を拒否しました`);
+        return;
+    };
+    const savePlayerData = { xuid: playerData.xuid, id: `${playerData.id}`, deviceId: [playerData.deviceSessionId] };
+    const playerRawDataBefore = player.getDynamicProperty("accountData") || JSON.stringify(savePlayerData);
+    /**
+     * @type {{ "deviceId": [string] , "id": string , "xuid": string }}
+     */
+    const playerParseDataBefore = JSON.parse(playerRawDataBefore);
+
+    if (!playerParseDataBefore.deviceId.includes(playerData.deviceSessionId)) {
+        playerParseDataBefore.deviceId.push(playerData.deviceSessionId);
+        player.setDynamicProperty("accountData", JSON.stringify(playerParseDataBefore));
+    };
+
+    if (!player.getDynamicProperty("accountData")) {
+        player.setDynamicProperty("accountData", JSON.stringify(playerParseDataBefore));
+    };
+
     if (unbanList.includes(`${player.name}`) && player.getDynamicProperty(`isBan`)) {
         player.setDynamicProperty(`isBan`);
         player.sendMessage(`§a§lあなたのBANが解除されました`);
         world.sendMessage(`§a§l[KaronNetWork BAN System]§r\n${player.name} §r§7のBANを解除しました`);
+        for (const deviceId of playerParseDataBefore.deviceId) {
+            if(deviceIds.includes(deviceId)) {
+                deviceIds.splice(deviceIds.indexOf(deviceId), 1);
+            };
+        };
+        DyProp.setDynamicProperty("deviceIds", JSON.stringify(deviceIds));
         return;
     };
+    for (const banedDeviceId of deviceIds) {
+        if (banedDeviceId === playerData.deviceSessionId) {
+            player.setDynamicProperty(`isBan`, true);
+            player.runCommand(`kick "${playerData.xuid}" §c§lあなたはBANされています\nReason: ${banedDeviceId}のデバイスからの接続を拒否しました`);
+            world.sendMessage(`§a§l[KaronNetWork BAN System]§r\n${player.name} §r§7の接続を拒否しました`);
+            if (!playerParseDataBefore.deviceId.includes(playerData.deviceSessionId)) {
+                deviceIds.push(playerData.deviceSessionId);
+                DyProp.setDynamicProperty("deviceIds", JSON.stringify(deviceIds));
+            };
+            return;
+        }
+    }
+
     if (banList.includes(`${player.name}`)) {
         const reason = player.getDynamicProperty(`banReason`) || "";
         player.setDynamicProperty(`isBan`, true);
         player.runCommand(`kick "${player.name}" §c§lあなたはBANされています\nReason: ${reason}`);
         world.sendMessage(`§a§l[KaronNetWork BAN System]§r\n${player.name} §r§7の接続を拒否しました`);
+        if (!playerParseDataBefore.deviceId.includes(playerData.deviceSessionId)) {
+            deviceIds.push(playerData.deviceSessionId);
+            DyProp.setDynamicProperty("deviceIds", JSON.stringify(deviceIds));
+        };
         return;
     };
     if (player.getDynamicProperty(`isBan`)) {
         const reason = player.getDynamicProperty(`banReason`) || "";
         player.runCommand(`kick "${player.name}" §c§lあなたはBANされています\nReason: ${reason}`);
         world.sendMessage(`§a§l[KaronNetWork BAN System]§r\n${player.name} §r§7の接続を拒否しました`);
+        if (!playerParseDataBefore.deviceId.includes(playerData.deviceSessionId)) {
+            deviceIds.push(playerData.deviceSessionId);
+            DyProp.setDynamicProperty("deviceIds", JSON.stringify(deviceIds));
+        };
         return;
     };
     if (muteList.includes(`${player.name}`)) {
@@ -43,7 +103,7 @@ world.afterEvents.playerSpawn.subscribe((ev) => {
  * 
  * @param {Player} player 
  */
-function banForm(player) {
+async function banForm(player) {
     const players = world.getPlayers();
     const playerNames = players.map(p => p.name);
     const form = new ModalFormData();
@@ -63,6 +123,18 @@ function banForm(player) {
              * @type {Player}
              */
             const target = players[rs.formValues[0]];
+            const playerRawDataBefore = target.getDynamicProperty("accountData");
+            /**
+             * @type {{ "deviceId": [string] , "id": string , "xuid": string }}
+             */
+            const playerParseDataBefore = JSON.parse(playerRawDataBefore);
+            for (let deviceId of playerParseDataBefore.deviceId) {
+                if (!deviceIds.includes(deviceId)) {
+                    deviceIds.push(deviceId);
+                };
+            };
+            DyProp.setDynamicProperty("deviceIds", JSON.stringify(deviceIds));
+
             if (rs.formValues[1] != "") target.setDynamicProperty(`banReason`, rs.formValues[1]);
             target.setDynamicProperty(`isBan`, true);
             target.runCommand(`kick "${target.name}" §c§lあなたはBANされています\nReason: ${rs.formValues[1]}`);
@@ -286,3 +358,77 @@ function inventoryCheck(player, target) {
         player.sendMessage(`§c無効なフィールド`);
     });
 };
+
+/**
+ * @type {{command: string, resolve: (value: string) => void, resultTmp: string}[]}
+ */
+const commandQueue = [];
+
+/**
+ * @template {boolean} T
+ * @param {string} command 
+ * @param {T} result 
+ * @returns {Promise<T extends true ? string : undefined>}
+ */
+export async function executeCommand(command = "", result = false) {
+    sendAction("execute", { command, result });
+    return new Promise((resolve) => {
+        if (result) commandQueue.push({ command, resolve, resultTmp: "" });
+        else resolve();
+    });
+}
+
+/**
+ * @type {{fullCommand: string, resolve: (value: {err:boolean,message:string}) => void, resultTmp: string}[]}
+ */
+const shellQueue = [];
+
+/**
+ * @template {boolean} T
+ * @param {string} command 
+ * @param {string[]} args
+ * @param {T} result 
+ * @returns {Promise<T extends true ? {err:boolean,message:string} : undefined>}
+ */
+export async function executeShellCommand(mainCommand = "", args = [], result = false) {
+    sendAction("executeshell", { main_command: mainCommand, args, result });
+    return new Promise((resolve) => {
+        if (result) shellQueue.push({ fullCommand: `${mainCommand} ${args.join(" ")}`, resolve, resultTmp: "" });
+        else resolve();
+    });
+}
+
+system.afterEvents.scriptEventReceive.subscribe((event) => {
+    if (event.id === "bds_enhancer:result") {
+        /**
+         * @type {{command: string, result_message: string, count: number, end: boolean}}
+         */
+        const data = JSON.parse(event.message);
+        const arr = commandQueue.find((item) => item.command === data.command)
+        if (!arr) return;
+        arr.resultTmp += data.result_message;
+        if (data.end) {
+            arr.resolve(arr.resultTmp);
+            commandQueue.splice(commandQueue.indexOf(arr), 1);
+        }
+    } else if (event.id === "bds_enhancer:shell_result") {
+        /**
+         * @type {{command: string, result_message: string, err: boolean, count: number, end: boolean}}
+         */
+        const data = JSON.parse(event.message);
+        if (data.err) {
+            const arr = shellQueue.find((item) => item.fullCommand === data.command)
+            if (!arr) return;
+            arr.resolve({ err: true, message: data.result_message });
+            shellQueue.splice(shellQueue.indexOf(arr), 1);
+        } else {
+            const arr = shellQueue.find((item) => item.fullCommand === data.command)
+            if (!arr) return;
+            arr.resultTmp += data.result_message;
+            if (data.end) {
+                arr.resolve({ err: false, message: arr.resultTmp });
+                shellQueue.splice(shellQueue.indexOf(arr), 1);
+            }
+        }
+    }
+});
