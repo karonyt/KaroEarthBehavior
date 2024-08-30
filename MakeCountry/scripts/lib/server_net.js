3252c27b3c975c07fc5518f67ed2147f6d403dfb
karonyt <karondaaa@gmail.com>
2024-08-31 00:38:42 +0900

    aa

--- MakeCountry/scripts/lib/server_net.js
+++ MakeCountry/scripts/lib/server_net.js
@@ -311,7 +311,7 @@ world.beforeEvents.chatSend.subscribe(event => {
 
 world.afterEvents.entityDie.subscribe(async (ev) => {
     const { deadEntity, damageSource } = ev;
-    //if (!killLogCheckEntityIds.includes(`${deadEntity?.typeId}`)) return;
+    if (!killLogCheckEntityIds.includes(`${deadEntity?.typeId}`)) return;
     try {
         const { x, y, z } = deadEntity.location;
         const req = new HttpRequest("http://localhost:20005/");
@@ -330,12 +330,16 @@ world.afterEvents.entityDie.subscribe(async (ev) => {
             new HttpHeader("Content-Type", "application/json")
         ];
         await http.request(req);
+        return;
     } catch (error) { };
 });
 
 world.afterEvents.playerPlaceBlock.subscribe(async (ev) => {
     const { player, block } = ev;
     const { x, y, z } = block.location;
+    const chunkData = GetAndParsePropertyData(GetPlayerChunkPropertyId(player));
+    if(!chunkData) return;
+    if(!chunkData?.countryId) return;
     const req = new HttpRequest("http://localhost:20005/");
     req.body = JSON.stringify({
         placeLog: true,
@@ -349,11 +353,15 @@ world.afterEvents.playerPlaceBlock.subscribe(async (ev) => {
         new HttpHeader("Content-Type", "application/json")
     ];
     await http.request(req);
+    return;
 });
 
 world.afterEvents.playerBreakBlock.subscribe(async (ev) => {
     const { player, brokenBlockPermutation, block } = ev;
     const { x, y, z } = block.location;
+    const chunkData = GetAndParsePropertyData(GetPlayerChunkPropertyId(player));
+    if(!chunkData) return;
+    if(!chunkData?.countryId) return;
     const req = new HttpRequest("http://localhost:20005/");
     req.body = JSON.stringify({
         breakLog: true,
@@ -367,4 +375,5 @@ world.afterEvents.playerBreakBlock.subscribe(async (ev) => {
         new HttpHeader("Content-Type", "application/json")
     ];
     await http.request(req);
+    return;
 });
