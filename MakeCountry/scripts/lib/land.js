f984b974b6eaff428b11f6714b7f311531f9a22d
karonyt <tomatotest34@gmail.com>
2024-09-15 11:53:30 +0900

    update land.js and war.js

--- MakeCountry/scripts/lib/land.js
+++ MakeCountry/scripts/lib/land.js
@@ -87,7 +87,7 @@ export function MakeCountry(owner, name = `country`, invite = true, peace = conf
         //加盟している国際組織
         internationalOrganizations: [],
         //戦争中
-        warNow: false,
+        isWarNow: false,
         //戦争中
         warNowCountries: [],
         //受け取った戦線布告の国
--- MakeCountry/scripts/lib/war.js
+++ MakeCountry/scripts/lib/war.js
@@ -1,6 +1,8 @@
 import { Player, system, world } from "@minecraft/server";
-import { GetAndParsePropertyData, GetPlayerChunkPropertyId, StringifyAndSavePropertyData } from "./util";
+import { GetAndParsePropertyData, GetChunkPropertyId, GetPlayerChunkPropertyId, StringifyAndSavePropertyData } from "./util";
 import config from "../config";
+
+const warCountry = new Map();
 
 /**
  * 
@@ -22,6 +24,10 @@ export function invade(player) {
         player.sendMessage({ rawtext: [{ text: `§a[MakeCountry]\n` }, { translate: `invade.error.peace` }] });
         return;
     };
+    if (playerCountryData?.isWarNow) {
+        player.sendMessage({ rawtext: [{ text: `§a[MakeCountry]\n` }, { translate: `invade.error.iswarnow` }] });
+        return;
+    };
     if (playerCountryData.alliance.includes(chunk.countryId)) {
         player.sendMessage({ rawtext: [{ text: `§a[MakeCountry]\n` }, { translate: `invade.error.alliance` }] });
         return;
@@ -34,16 +40,20 @@ export function invade(player) {
     const date = new Date().getTime();
     const cooltime = playerCountryData?.invadeCooltime ?? date - 1000;
     if (cooltime - date > 0) {
-        player.sendMessage({ rawtext: [{ text: `§a[MakeCountry]\n` }, { translate: `invade.error.cooltime` , with: [`${Math.ceil((cooltime - date) / 100) / 10}`]}] });
+        player.sendMessage({ rawtext: [{ text: `§a[MakeCountry]\n` }, { translate: `invade.error.cooltime`, with: [`${Math.ceil((cooltime - date) / 100) / 10}`] }] });
         return;
     };
 
     playerCountryData.invadeCooltime = date + (config.invadeCooltime * 1000);
     playerCountryData.peaceChangeCooltime = 7;
-
     //平和主義
     //モブ出す
     //クールタイム
     //切り替えれないように変更
-    StringifyAndSavePropertyData(`country_${playerCountryData.id}`,playerCountryData);
+    const coreEntity = player.dimension.spawnEntity(`mc:core`, player.getHeadLocation());
+    coreEntity.nameTag = `${targetCountryData.name}§r Core`;
+    let chunkmsg = GetPlayerChunkPropertyId(player).split(/(?<=^[^_]+?)_/)[1];
+    const msg = chunkmsg.replace(/_/g,` `);
+    world.sendMessage({ rawtext: [{ text: `§a[MakeCountry]\n` }, { translate: `invade.success`, with: [`${player.name}§r(${playerCountryData.name}§r)`, `${msg}§r(${playerCountryData.name}§r`] }] })
+    StringifyAndSavePropertyData(`country_${playerCountryData.id}`, playerCountryData);
 };
